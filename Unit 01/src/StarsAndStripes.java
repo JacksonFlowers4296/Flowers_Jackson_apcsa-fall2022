da214a5781e93c6f2ddc7fef495b56b3477a876b
Jackson Flowers <111385086+JacksonFlowers4296@users.noreply.github.com>
2022-08-22 11:10:25 -0700

    Add files via upload
    Here are my files for the unit 01 project

---
+++ Unit 01/src/StarsAndStripes.java
@@ -0,0 +1,68 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+   }
+
+   public void printTwentyStars()
+   {
+	   out.println("* * * * * * * * * * ");
+	   out.println("* * * * * * * * * * ");
+	   out.println("                    ");
+   }
+
+   public void printTwentyDashes()
+   {
+	   out.println("----------");
+	   out.println("----------");
+   }
+
+   public void printTwoBlankLines()
+   {
+	   out.println("          ");
+	   out.println("          ");
+   }
+   
+   public void printASmallBox()
+   {	
+	   out.println("__________");
+	   out.println("\\||||||//");
+	   out.println("//||||||\\");
+	   out.println("\\||||||//");
+	   out.println("//||||||\\");
+	   out.println("[][][][][]");
+	   out.println("           ");
+   }
+ 
+   public void printABigBox()
+   { 	
+	   out.println("[][][][][][][][][][][][][][][][]");
+	   out.println("@#$@#$@#$@#$@#$@#$@#$@#$@#$@#$@#");
+	   out.println("^7^7^7^7^7^7^7^7^7^7^7^7^7^7^7^7");
+	   out.println("<><><><><><><><><><><><><><><><>");
+	   out.println("[][][][][][][][][][][][][][][][]");
+	   out.println("@#$@#$@#$@#$@#$@#$@#$@#$@#$@#$@#");
+	   out.println("^7^7^7^7^7^7^7^7^7^7^7^7^7^7^7^7");
+	   out.println("<><><><><><><><><><><><><><><><>");
+	   out.println("[][][][][][][][][][][][][][][][]");
+	   out.println("@#$@#$@#$@#$@#$@#$@#$@#$@#$@#$@#");
+	   out.println("^7^7^7^7^7^7^7^7^7^7^7^7^7^7^7^7");
+	   out.println("<><><><><><><><><><><><><><><><>");
+	   out.println("[][][][][][][][][][][][][][][][]");
+	   out.println("@#$@#$@#$@#$@#$@#$@#$@#$@#$@#$@#");
+	   out.println("^7^7^7^7^7^7^7^7^7^7^7^7^7^7^7^7");
+	   out.println("<><><><><><><><><><><><><><><><>");
+   }   
+}
