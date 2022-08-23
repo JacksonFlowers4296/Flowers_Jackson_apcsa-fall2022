d14cdaa347e429096f1cdb3a8126b97acd5a3abd
Jackson Flowers <111385086+JacksonFlowers4296@users.noreply.github.com>
2022-08-22 18:03:29 -0700

    All 3 labs
    Here are my three Unit-02 labs

---
+++ Unit-02/src/Trap.java
@@ -0,0 +1,14 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name - Jackson flowers
+//Date - 8/22/22
+//Class - AP CSA
+//Lab  - Unit 02 Trapezoid
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		return ((double)(base1 + base2)/2 * height);
+		//The Double was the most important part of the equation
+	}
+}
