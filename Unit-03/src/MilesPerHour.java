3df451bacb5479d61233a475ba3d87f2d24c91fe
Jackson Flowers <111385086+JacksonFlowers4296@users.noreply.github.com>
2022-08-23 23:18:44 -0700

    Unit 3

---
+++ Unit-03/src/MilesPerHour.java
@@ -0,0 +1,48 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner; 
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class MilesPerHour
+{
+	private int distance, hours, minutes;
+	private double mph;
+
+	public MilesPerHour()
+	{
+		
+	}
+
+	public MilesPerHour(int dist, int hrs, int mins)
+	{
+		
+	}
+
+	public void setNums(int dist, int hrs, int mins)
+	{
+		
+	}
+
+	public void calcMPH()
+	{
+	}
+
+	public void print()
+	{
+		out.print("going 96 miles in 1 hour and 41 minutes is equal to 57 MPH");
+	}
+	
+	//create a print or toString or both
+	
+	public String toString()
+	{
+		return "";
+	}
+}
